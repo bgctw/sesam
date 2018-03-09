cda5cc620886fa8642497cf62a8046d903faf43c
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2018-03-09 14:06:00 +0100

    started P implementation

---
+++ R/sesam3P.R
@@ -0,0 +1,244 @@
+#library(deSolve)
+
+# gC/m2 and gN/m2, /yr
+
+derivSesam3a <- function(
+  ### Soil Enzyme Steady Allocation model
+  t,x,parms
+){
+  ##details<<
+  ## Simplified version of sesam3s
+  ## model corresponding to Seam3 with enzyme levels computed by quasi steady state
+  ## Alpha as an explicit state variable that changes with turnover
+  ## Simplified computation of target based on current revenue based on current alpha
+  x <- pmax(unlist(x),1e-16)      # no negative masses
+  # compute steady state enzyme levels for N and for C limitation
+  dRPot <- parms$kR * x["R"]
+  dLPot <- parms$kL * x["L"]
+  immoPot <- parms$iB * x["I"]
+  immoPPot <- parms$iBP * x["I"]
+  cnR <- x["R"]/x["RN"]
+  cnL <- x["L"]/x["LN"]
+  cnE <- parms$cnE
+  cnB <- parms$cnB
+  cpR <- x["R"]/x["RP"]
+  cpL <- x["L"]/x["LP"]
+  cpE <- parms$cpE
+  cpB <- parms$cpB
+  alpha <- x["alpha"]
+  B <- x["B"]
+  aeB <- parms$aE*B        # aeB without associanted growth respiration
+  kmN <- parms$km*parms$kN
+  rM <- parms$m*B          # maintenance respiration
+  tvrB <- parms$tau*B      # microbial turnover
+  synE <- if (isTRUE(parms$isEnzymeMassFlux)) aeB else 0
+  #
+  # enzyme limitations of decomposition
+  decL <- dLPot * (1 - alpha)*aeB/(kmN + (1 - alpha)*aeB)
+  decR <- dRPot * alpha*aeB/(kmN + alpha*aeB)
+  #
+  tvrERecycling <- parms$kNB*synE
+  uNOrg <- parms$nu*(decL/cnL + decR/cnR + tvrERecycling/cnE)
+  uPOrg <- parms$nuP*(decL/cpL + decR/cpR + tvrERecycling/cpE)
+  uC <- decL + decR + tvrERecycling
+  CsynBC <- uC - rM - synE/parms$eps
+  CsynBN <- cnB/parms$eps*(uNOrg + immoPot - synE/cnE)
+  CsynBP <- cpB/parms$eps*(uPOrg + immoPPot - synE/cpE)
+  CsynB <- min(CsynBC, CsynBN, CsynBP)
+  if (CsynB > 0) {
+    synB <- parms$eps*CsynB
+    rG <- (1 - parms$eps)*CsynB
+  } else {
+    synB <- CsynB # with negative biomass change, do growth respiration
+    rG <- 0
+  }
+  PhiB <- uNOrg - synB/cnB - synE/cnE
+  PhiBP <- uPOrg - synB/cpB - synE/cpE
+  alphaC <- computeSesam3sAllocationPartitioning(
+    dR = dRPot, dL = dLPot, B = B
+    , kmkN = kmN, aE = parms$aE
+    , alpha = alpha
+  )
+  alphaN <- computeSesam3sAllocationPartitioning(
+    dR = dRPot/cnR, dL = dLPot/cnL, B = B
+    , kmkN = kmN, aE = parms$aE
+    , alpha = alpha
+  )
+  alphaP <- computeSesam3sAllocationPartitioning(
+    dR = dRPot/cpR, dL = dLPot/cpL, B = B
+    , kmkN = kmN, aE = parms$aE
+    , alpha = alpha
+  )
+  alphaTarget <- balanceAlphaBetweenCNLimitations(
+    alphaC, alphaN, CsynBN, CsynBC
+    , NsynBC = parms$eps*CsynBC/cnB
+    , NsynBN = parms$eps*CsynBN/cnB)
+  # microbial community change as fast as microbial turnover
+  dAlpha <- (alphaTarget - alpha) * parms$tau
+  #
+  # imbalance fluxes of microbes and predators (consuming part of microbial turnover)
+  respO <- uC - (synE/parms$eps + synB/parms$eps + rM)
+  respTvr <- (1 - parms$epsTvr) * tvrB
+  # assuming same cnRatio of predators to equal cn ratio of microbes
+  PhiTvr <- respTvr/parms$cnB
+  #
+  # tvr that feeds R pool, assume that N in SOM for resp (by epsTvr) is mineralized
+  tvrC <-  +parms$epsTvr*tvrB   + (1 - parms$kNB)*synE
+  tvrN <-  +parms$epsTvr*tvrB/parms$cnB   + (1 - parms$kNB)*synE/parms$cnE
+  # fluxes leaving the system (will be set in scen where trv does not feed back)
+  tvrExC <- tvrExN <- 0
+  #
+  leach <- parms$l*x["I"]
+  PhiU <- (1 - parms$nu)*(decL/cnL + decR/cnR + tvrERecycling/cnE)
+  #
+  dB <- synB - tvrB
+  dL <- -decL  + parms$iL
+  dLN <- -decL/cnL   + parms$iL/parms$cnIL
+  dR <- -decR  + parms$iR  + tvrC
+  dRN <- -decR/cnR  + parms$iR/parms$cnIR  + tvrN
+  # here plant uptake as absolute parameter
+  dI <-  +parms$iI  - parms$kIP  - leach  + PhiU  + PhiB  + PhiTvr
+  #
+  if (isTRUE(parms$isFixedS)) {
+    # scenario of fixed substrate
+    dR <- dL <- dRN <- dLN <- dI <- 0
+  } else if (isTRUE(parms$isTvrNil)) {
+    # scenario of enzymes and biomass not feeding back to R
+    dR <- +parms$iR - decR
+    dRN <- +parms$iR/parms$cnIR - decR/cnR
+    tvrExC <- tvrC
+    tvrExN <- tvrN
+  }
+  #
+  resDeriv <- structure(as.numeric(
+    c( dB, dR, dRN, dL, dLN, dI, dAlpha))
+    ,names = c("dB","dR","dRN","dL","dLN","dI","dAlpha"))
+  if (any(!is.finite(resDeriv))) stop("encountered nonFinite derivatives")
+  sqrEps <- sqrt(.Machine$double.eps)
+  # parms$iL - (decL + dL)
+  # parms$iR + tvrC -(decR + dR)
+  #
+  # checking the mass balance of fluxes
+  plantNUp <- 0 # checked in mass balance but is not (any more) in model
+  respB <- (synE)/parms$eps*(1 - parms$eps)  + rG + rM + respO
+  resp <- respB + respTvr
+  if (diff( unlist(c(uC = uC, usage = respB + synB + synE )))^2 > sqrEps )  stop(
+    "biomass mass balance C error")
+  if (diff( unlist(
+    c(uN = uNOrg, usage = synE/parms$cnE + synB/parms$cnB + PhiB )))^2 >
+    .Machine$double.eps)  stop("biomass mass balance N error")
+  if (!isTRUE(parms$isFixedS)) {
+    if (diff(unlist(
+      c(dB + dR + dL + tvrExC + resp,    parms$iR + parms$iL )))^2 >
+      sqrEps )  stop("mass balance C error")
+    if (diff(unlist(
+      c( dB/parms$cnB  + dRN + dLN + dI + tvrExN
+         , parms$iR/parms$cnIR  + parms$iL/parms$cnIL - plantNUp  + parms$iI -
+         parms$kIP - parms$l*x["I"])))^2 >
+      .Machine$double.eps )  stop("mass balance dN error")
+  }
+  #
+  # allowing scenarios with holding some pools fixed
+  if (isTRUE(parms$isFixedR)) { resDeriv["dR"] <- resDeriv["dRN"] <-  0   }
+  if (isTRUE(parms$isFixedL)) { resDeriv["dL"] <- resDeriv["dLN"] <-  0   }
+  if (isTRUE(parms$isFixedI)) { resDeriv["dI"] <-  0   }
+  #
+  # further computations just for output for tacking the system
+  ER <- alpha * parms$aE * x["B"] / parms$kN
+  EL <- (1 - alpha) * parms$aE * x["B"] / parms$kN
+  limER <- ER / (parms$kmR + ER)
+  limEL <- EL / (parms$kmL + EL)
+  revRC <- dRPot / (parms$km*parms$kN + alphaC*aeB)
+  revLC <- dLPot / (parms$km*parms$kN + (1 - alphaC)*aeB)
+  revRN <- dRPot/cnR / (parms$km*parms$kN + alphaN*aeB)
+  revLN <- dLPot/cnL / (parms$km*parms$kN + alphaN*aeB)
+  # net mic mineralization/immobilization when accounting uptake mineralization
+  PhiBU <- PhiB + PhiU
+  # total mineralization flux including microbial turnover
+  PhiTotal <- PhiBU + PhiTvr
+  # do not match in other limitation
+  # c(alphaC, revRC/(revRC + revLC)); c(alphaN, revRN/(revRN + revLN))
+  # compute C available for biomass, this time without accounting immobilization flux
+  NsynBNSubstrate <- uNOrg - synE/cnE
+  CsynBNSubstrate <- (NsynBNSubstrate*cnB)/parms$eps
+  # N limited based on substrate uptake (without accounting for immobilization)
+  isLimNSubstrate <-  CsynBNSubstrate < CsynBC
+  #
+  if (isTRUE(parms$isRecover) ) recover()
+  list( resDeriv, c(
+     resp = as.numeric(resp)
+     , respO = as.numeric(respO)
+     , respB = as.numeric(respB)
+     , respTvr = as.numeric(respTvr)
+     #, ER = as.numeric(ER), EL = as.numeric(EL)
+    #, MmB = as.numeric(MmB)
+    , PhiTotal = as.numeric(PhiTotal)
+    , PhiB = as.numeric(PhiB), PhiU = as.numeric(PhiU)
+    , PhiTvr = as.numeric(PhiTvr)
+    , PhiBU = as.numeric(PhiBU)
+    , immoPot = as.numeric(immoPot)
+    , alphaTarget = as.numeric(alphaTarget)
+    , alphaC = as.numeric(alphaC), alphaN = as.numeric(alphaN)
+    , cnR = as.numeric(cnR), cnL = as.numeric(cnL)
+    , limER = as.numeric(limER), limEL = as.numeric(limEL)
+    , decR = as.numeric(decR), decL = as.numeric(decL)
+    , tvrB = as.numeric(tvrB)
+    , revRC = as.numeric(revRC), revLC = as.numeric(revLC)
+    , revRN = as.numeric(revRN)
+    , revLN = as.numeric(revLN)
+    , CsynB = as.numeric(CsynB)
+    , CsynBC = as.numeric(CsynBC)
+    , CsynBN = as.numeric(CsynBN)
+    #, pNsyn = as.numeric(NsynBN / (parms$eps*CsynBC/cnB) )
+    #, NsynReq = as.numeric(CsynBC/cnB), Nsyn = as.numeric(NsynBN)
+    #, dR = as.numeric(dR), dL = as.numeric(dL), dB = as.numeric(dB)
+    #, dI = as.numeric(dI)
+    #, uC = as.numeric(uC), synB = as.numeric(synB)
+    #, decN = as.numeric(decN)
+  ))
+}
+
+.depr.balanceAlphaBetweenElementLimitations <- function(
+  ### compute balance between alphas of different element limitations
+  alpha    ##<< numeric vector of allocation coefficients for different elements
+  , CsynBE ##<< numeric vector of carbon availale for biomass synthesis
+  ## for each based on element limitaiton, first is carbon
+  , ce     ##<< numeric vector for C to E microbial biomass ratios
+  ## The first entry C to C is not considered.
+  , eps    ##<< numeric scalar: intrinsic carbon use eff. for biomass synthesis
+  , delta = 200  ##<< scalar smoothing factor
+  #, alphaC, alphaN, CsynBN, CsynBC, NsynBC, NsynBN
+){
+  ##details<< Select the alpha corresponding to the smallest CsynBE.
+  ## However, if elemental limitations are close,
+  ## do a smooth transition between corresponding alpha values
+  wCLim = min( .Machine$double.xmax, (min(CsynBE[-1]/CsynBE[1]))^delta )
+  NSynBE <- eps*CsynBE[2]/ce[2]
+  wNLim = min( .Machine$double.xmax, (min(CsynBE[-2]/CsynBE[2]))^delta )
+  ce[1] <- 1/eps  # so that EsynBE equals CSynBE for first component
+  wELim <- sapply( seq_along(CsynBE), function(iE){
+    EsynBE <- eps*CsynBE/ce[iE]  # available mass of E for biomass synthesis
+    # min to avoid  + Inf
+    wELimi <- min( .Machine$double.xmax, (min(EsynBE[-iE]/EsynBE[iE]))^delta )
+  })
+  #alpha <- (wCLim*alphaC + wNLim*alphaN) / (wCLim + wNLim)
+  alphaBalanced <- sum(wELim * alpha)/sum(wELim)
+  alphaBalanced
+}
+
+balanceAlphaBetweenElementLimitations <- function(
+  ### compute balance between alphas of different element limitations
+  alpha    ##<< numeric vector of allocation coefficients for different elements
+  , CsynBE ##<< numeric vector of carbon availale for biomass synthesis
+  , delta = 200  ##<< scalar smoothing factor
+){
+  ##details<< Select the alpha corresponding to the smallest CsynBE.
+  ## However, if elemental limitations are close,
+  ## do a smooth transition between corresponding alpha values
+  wELim <- sapply( seq_along(CsynBE), function(iE){
+    # min to avoid  + Inf
+    wELimi <- min( .Machine$double.xmax, (min(CsynBE[-iE]/CsynBE[iE]))^delta )
+  })
+  alphaBalanced <- sum(wELim * alpha)/sum(wELim)
+  alphaBalanced
+}
---
+++ tests/testthat/test_modSesam3P.R
@@ -0,0 +1,289 @@
+#require(testthat)
+#test_file("tests/testthat/test_modSesam3.R")
+context("modSesam3P")
+
+parms0 <- list(
+  cnB = 7.16
+  ,cnE = 3.1     # Sterner02: Protein (Fig. 2.2.), high N investment (low P)
+  #,cnE = 7.16
+  ,cnIR = 4.5     ##<< between micr and enzyme signal
+  ,cnIL = 30      ##<< N poor substrate
+  #,kN = 0.05     ##<< (per day) enzyme turnover
+  ,kN = 0.01*365  ##<< /yr enzyme turnover 1% turning over each day
+  ,kNB = 0.8      ##<< amount of recycling enzyme turnover by biomass (added to uptake instead of R)
+  #,kR = 0.2      ##<< substrate decomposition rate N-rich (here simulate large N stock)
+  #,kL = 1        ##<< substrate decomposition rate N-poor
+  #,kR = 5e-3     ##<< substrate decomposition rate N-rich (here simulate large N stock)
+  #,kL = 10e-3    ##<< substrate decomposition rate N-poor
+  #,kL = 5e-2     ##<< substrate decomposition rate N-poor
+  #,aE = 0.05     ##<< C-uptake allocated to enzymes
+  #,kR = 1/(5*365)      ##<< 5 years
+  #,kL = 1/(0.5*365)    ##<< 1/2 years
+  ,kR = 1/(50)    ##<< 1/(x years)
+  ,kL = 1/(1)     ##<< 1/(x years)
+  #,aE = 0.003*365 ##<< C biomass allocated to enzymes gC/day /microbial biomass
+  ,aE = 0.001*365 ##<< C biomass allocated to enzymes gC/day /microbial biomass
+  ,km = 0.3       ##<< enzyme half-saturation constant
+  #,km = 0.03     ##<< enzyme half-saturation constant
+  #,km = 14       ##<< enzyme half-saturation constant
+  ,m = 0.02*365   ##<< maintenance respiration rate   gC/day /microbial biomass
+  ,tau = 1/60*365 ##<< biomass turnover rate (12 days)
+  ,eps = 0.5      ##<< carbon use efficiency
+  ,epsTvr = 0.3   ##<< carbon use efficiency of microbial tvr (predators respire)
+  ,iR = 0         ##<< input modelled explicitely
+  ,iL = 300       ##<< g/m2 input per year (half NPP)
+  #,plantNUp = 300/70*1/4  ##<< plant N uptake balancing N inputs
+  ,plantNUp = 0   ##<< plant N uptake balancing N inputs
+  ,useFixedAlloc = FALSE    ##<< set to true to use fixed enzyme allocation (alpha = 0.5)
+  ,kIP = 10.57 #0.0289652*365         ##<< plant uptake iP I
+  ,iB = 0.38 * 10.57 #0.0110068*365   ##<< immobilization flux iB I
+  ,iI = 0         ##<< input of mineral N
+  ,l = 0.96 #0.00262647*365       ##<< leaching rate of mineralN l I
+  , nu = 0.9       ##<< microbial N use efficiency
+  #, isEnzymeMassFlux = FALSE  ##<< steady state B solution neglects enyzme mass fluxes
+  , isEnzymeMassFlux = TRUE  ##<< steady state B solution accounts for enyzme mass fluxes
+  , nuP = 0.3      ##<< microbial uptake of depolymerized P, (1-nuP) is mineralized
+  , cpB = 40
+  , cpE = 50
+  , iBP = 0.38 * 10.57 # start with same as N
+
+)
+parms0 <- within(parms0,{
+  kmR <- kmL <- km
+  eps1 <- eps2 <- eps
+  cnER <- cnEL <- cnE
+  kNR <- kNL <- kN
+  kIP <- iL / cnIL	# same litter input as plant uptake
+  kIP <- 0			# no plant uptake
+})
+
+parms <- parms0
+
+x0 <- x0Orig <- c( #aE = 0.001*365
+  B = 20                     ##<< microbial biomass
+  , R = 7000                 ##<< N rich substrate
+  , RN = 7000/parms0$cnIR    ##<< N rich substrate N pool
+  , L = 200                  ##<< N poor substrate
+  , LN = 200/parms0$cnIL     ##<< N poor substrate N pool
+  , I =  1                   ##<< inorganic pool
+  , alpha = 0.5              ##<< initial community composition
+)
+x <- x0
+
+# for SEAM model add enzyme state variables
+x0Seam3 <- c(x0
+             , ER  = 1.5*parms0$km                  ##<< total enzyme pool
+             , EL  = 1.5*parms0$km                   ##<< total enzyme pool
+             # make sure to have same order as derivative of Seam3
+)[c("B","ER","EL","R","RN","L","LN","I","alpha")]
+#x0Seam3
+
+x0Nlim <- c( #aE = 0.001*365
+  B = 20                     ##<< microbial biomass
+  , R = 1000                 ##<< N rich substrate
+  , RN = 1000/parms0$cnIR    ##<< N rich substrate N pool
+  , L = 200                  ##<< N poor substrate
+  , LN = 200/parms0$cnIL     ##<< N poor substrate N pool
+  , I =  0                   ##<< inorganic pool
+  , alpha = 0.5              ##<< initial community composition
+)
+x0NlimSeam3 <- c(x0Nlim
+                 , ER  = 1.5*parms0$km                  ##<< total enzyme pool
+                 , EL  = 1.5*parms0$km                   ##<< total enzyme pool
+                 # make sure to have same order as derivative of Seam3
+)[c("B","ER","EL","R","RN","L","LN","I","alpha")]
+
+test_that("balanceAlphaBetweenElementLimitations",{
+  #balanceAlphaBetweenCNLimitations()
+  scen <- "C - limited"
+  alpha <- c(alphaC = 0.2, alphaN = 0.6, alphaP = 0.8)
+  CsynBE <- c(CsynBC = 40, CsynBN = 6000, CSynBP = 6000)
+  ce <- c(ccB = NA, cnB = 8, cnP = 8*8)
+  eps <- 0.5
+  alphaBalanced <- balanceAlphaBetweenElementLimitations(
+    alpha, CsynBE)#, ce, eps  )
+  expect_equal(alphaBalanced, as.numeric(alpha[1]), tolerance = 1e-2)
+  #
+  scen <- "N - limited"
+  CsynBE <- c(CsynBC = 6000, CsynBN = 40, CSynBP = 6000)
+  alphaBalanced <- balanceAlphaBetweenElementLimitations(
+    alpha, CsynBE)#, ce, eps  )
+  expect_equal(alphaBalanced, as.numeric(alpha[2]), tolerance = 1e-2)
+  #
+  scen <- "P - limited"
+  CsynBE <- c(CsynBC = 6000, CsynBN = 6000, CSynBP = 40)
+  alphaBalanced <- balanceAlphaBetweenElementLimitations(
+    alpha, CsynBE)#, ce, eps  )
+  expect_equal(alphaBalanced, as.numeric(alpha[3]), tolerance = 1e-2)
+  #
+  scen <- "CN - limited"
+  CsynBE <- c(CsynBC = 40, CsynBN = 40, CSynBP = 6000)
+  alphaBalanced <- balanceAlphaBetweenElementLimitations(
+    alpha, CsynBE)#, ce, eps  )
+  expect_equal(alphaBalanced, as.numeric(mean(alpha[1:2])), tolerance = 1e-2)
+  #
+  scen <- "CN - limited, slightly more N limted"
+  CsynBE <- c(CsynBC = 40, CsynBN = 40 - 1e-1, CSynBP = 6000)
+  alphaBalanced <- balanceAlphaBetweenElementLimitations(
+    alpha, CsynBE)#, ce, eps  )
+  alphaBalanced
+  expect_true(alphaBalanced > alpha[1] & alphaBalanced < alpha[2])
+  #
+  scen <- "NP - limited"
+  CsynBE <- c(CsynBC = 6000, CsynBN = 40, CSynBP = 40)
+  alphaBalanced <- balanceAlphaBetweenElementLimitations(
+    alpha, CsynBE)#, ce, eps  )
+  expect_equal(alphaBalanced, as.numeric(mean(alpha[2:3])), tolerance = 1e-2)
+  #
+  scen <- "NP - limited"
+  CsynBE <- c(CsynBC = 6000, CsynBN = 40, CSynBP = 40)
+  alphaBalanced <- balanceAlphaBetweenElementLimitations(
+    alpha, CsynBE)#, ce, eps  )
+  expect_equal(alphaBalanced, as.numeric(mean(alpha[2:3])), tolerance = 1e-2)
+  #
+  .tmp.f <- function(){
+     epsNs <- seq(-0.4,+0.4, length.out = 51)
+     alphaBs <- sapply(epsNs, function(epsN){
+       CsynBE <- c(CsynBC = 40, CsynBN = 40 - epsN, CSynBP = 6000)
+       alphaBalanced <- balanceAlphaBetweenElementLimitations(
+         alpha, CsynBE)#, ce, eps  )
+     })
+     plot(alphaBs ~ epsNs, type = "l")
+  }
+  #
+  scen <- "strongly C - limited"
+  # negative carbon balance
+  alpha <- c(alphaC = 0.2, alphaN = 0.6, alphaP = 0.8)
+  CsynBE <- c(CsynBC = -40, CsynBN = 4, CSynBP = 4)
+  ce <- c(ccB = NA, cnB = 8, cnP = 8*8)
+  eps <- 0.5
+  alphaBalanced <- balanceAlphaBetweenElementLimitations(
+    alpha, CsynBE)#, ce, eps  )
+  alphaBalanced
+  # think of accounting for negative biomass balance
+  expect_equal(alphaBalanced, as.numeric(alpha[1]), tolerance = 1e-2)
+  #
+
+})
+
+test_that("same as seam for fixed substrates", {
+  parmsFixedS <- within(parms0,{
+    isFixedS <- TRUE
+  })
+  parmsTvr0 <- within(parms0,{
+    isTvrNil <- TRUE
+    iR <- 160
+  })
+  times <- seq(0, 2100, length.out = 2)
+  #times <- seq(0,2100, length.out = 101)
+  resTest <- as.data.frame(lsoda( x0[-1], times, derivSesam3B, parms = parmsFixedS))
+  resExp <- as.data.frame(lsoda(
+    x0, times, derivSesam3s
+    , parms = parmsFixedS))
+    #, parms = within(parmsFixedS, isEnzymeMassFlux <- FALSE)))
+  xETest <- unlist(tail(resTest,1))
+  xEExp <- unlist(tail(resExp,1))
+  expect_equal( xETest["alphaC"], xEExp["alphaC"], tolerance = 1e-6)
+  expect_equal( xETest[2:7], xEExp[3:8], tolerance = 1e-6)
+  .tmp.f <- function(){
+    derivSesam3B(0, xETest[c(2:7)], within(parmsFixedS, isRecover <- TRUE))
+    derivSesam3B(0, xEExp[c(3:8)], within(parmsFixedS, isRecover <- TRUE))
+    derivSesam3s(0, xEExp[c(2:8)], within(parmsFixedS, isRecover <- TRUE))
+  }
+  #
+  # N limitation
+  #times <- seq(0,2100, length.out = 101)
+  resTest <- as.data.frame(lsoda( x0Nlim[-1], times, derivSesam3B, parms = parmsFixedS))
+  resExp <- as.data.frame(lsoda(
+    x0Nlim, times, derivSesam3s
+    , parms = parmsFixedS))
+    #, parms = within(parmsFixedS, isEnzymeMassFlux <- FALSE)))
+  xETest <- unlist(tail(resTest,1))
+  xEExp <- unlist(tail(resExp,1))
+  expect_equal( xETest["B"], xEExp["B"], tolerance = 1e-6)
+  expect_equal( xETest["alpha"], xEExp["alpha"], tolerance = 1e-6)
+  expect_equal( xETest["alphaN"], xEExp["alphaN"], tolerance = 1e-6)
+  expect_equal( xETest[2:7], xEExp[3:8], tolerance = 1e-6)
+})
+
+test_that("same as sesam2 with substrate feedbacks", {
+  parmsInit <- within(parms0, {isFixedI <- TRUE})
+  times <- seq(0,800, length.out = 2)
+  #times <- seq(0,800, length.out = 101)
+  #times <- c(0,148:151)
+  #times <- seq(0,2100, by = 2)
+  #times <- seq(0,10000, length.out = 101)
+  #ans1 <- derivSesam3B(0, x0[-1], within(parmsInit, isRecover <- TRUE) )
+  ans1 <- derivSesam3B(0, x0[-1], parmsInit)
+  expect_equal(ans1[[2]]["dB"], c(dB = 0), tolerance = 1e-6)
+  #
+  resTest <- as.data.frame(lsoda( x0[-1], times, derivSesam3B, parms = parmsInit))
+  resExp <- as.data.frame(lsoda(
+    x0, times, derivSesam3s
+    , parms = parmsInit))
+  #    , parms = within(parmsInit, isEnzymeMassFlux <- FALSE)))
+  xETest <- unlist(tail(resTest,1))
+  xEExp <- unlist(tail(resExp,1));
+  xpESteady <- unlist(head(tail(resTest,2),1))	# the previous before end
+  expect_equal( xETest["alphaC"], xEExp["alphaC"], tolerance = 1e-4)
+  expect_equal( xETest[2:7], xEExp[c(3:8)], tolerance = 1e-6)
+  #rbind(xEExp[names(xETest)], xETest)
+  .tmp.f <- function(){
+    derivSesam3B(0, x0[-1], within(parmsInit, isRecover <- TRUE))
+  }
+  #
+  # N limitation
+  resTest <- as.data.frame(lsoda( x0Nlim[-1], times, derivSesam3B, parms = parmsInit))
+  resExp <- as.data.frame(lsoda(
+    x0Nlim, times, derivSesam3s
+    , parms = parmsInit))
+    #, parms = within(parmsInit, isEnzymeMassFlux <- FALSE)))
+  xETest <- unlist(tail(resTest,1))
+  xEExp <- unlist(tail(resExp,1))
+  expect_equal( xETest["alpha"], xEExp["alpha"], tolerance = 1e-6)
+  expect_equal( xETest["alphaN"], xEExp["alphaN"], tolerance = 1e-6)
+  expect_equal( xETest[2:7], xEExp[c(3:8)], tolerance = 1e-6)
+  #rbind(xEExp[names(xETest)], xETest)
+  #
+  # from C to N limitation
+  x0CNLim <- x0; x0CNLim["I"] <- 0
+  x0CNLimSeam3 <- x0Seam3; x0CNLimSeam3["I"] <- 0
+  times <- seq(0,1200, length.out = 2)
+  #times <- seq(0,1200, length.out = 101)
+  #times <- c(0,seq(140,220, length.out = 101))
+  resTest <- as.data.frame(lsoda( x0CNLim[-1], times, derivSesam3B, parms = parmsInit))
+  resExp <- as.data.frame(lsoda(
+    x0CNLim, times, derivSesam3s
+    , parms = parmsInit))
+    #, parms = within(parmsInit, isEnzymeMassFlux <- FALSE)))
+  xETest <- unlist(tail(resTest,1))
+  xEExp <- unlist(tail(resExp,1))
+  expect_equal( xETest["B"], xEExp["B"], tolerance = 1e-4)
+  expect_equal( xETest["alphaN"], xEExp["alphaN"], tolerance = 1e-5)
+  expect_equal( xETest[2:7], xEExp[c(3:8)], tolerance = 1e-4)
+  rbind(xEExp[names(xETest)], xETest)
+})
+
+.tmp.f <- function(){
+  # plots of results
+  library(dplyr)
+  library(ggplot2)
+  res <- suppressWarnings(bind_rows(
+    cbind(resTest, scen = "Test"), cbind(resExp, scen = "Exp")))
+  ggplot(filter(res, time > 1), aes(time, B, color = scen)) + geom_line(alpha = 0.5)
+  ggplot(filter(res, time > 0), aes(time, alpha, color = scen)) + geom_point(alpha = 0.5)
+  ggplot(filter(res, time < 500 & time > 0), aes(time, alpha, color = scen)) + geom_point()
+  ggplot(filter(res, time < 500), aes(time, B, color = scen)) + geom_line()
+  ggplot(filter(res, time >= 0), aes(time, L, color = scen)) + geom_line()
+  ggplot(filter(res, time >= 0), aes(time, R, color = scen)) + geom_line()
+  ggplot(filter(res, time < 500), aes(time, respO, color = scen)) + geom_line()
+  ggplot(filter(res, time > 10 & time < 500), aes(time, ER, color = scen)) + geom_line()
+  ggplot(filter(res, time > 10 & time < 500), aes(time, EL, color = scen)) + geom_line()
+  ggplot(filter(res, time < 500), aes(time, alphaC, color = scen)) + geom_line()
+  ggplot(filter(res, time < 5000), aes(time, alphaN, color = scen)) + geom_line()
+  ggplot(filter(res, time > 01), aes(time, PhiB, color = scen, linetype = scen)) + geom_line()
+}
+
+
+
+
