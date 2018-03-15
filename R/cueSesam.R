c50a3db38253ae2613c06e2c14d6a507afa6acea
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2018-03-15 17:47:05 +0100

    litter bag/priming study
    increased turnover by predation yields more realistic priming scenario.
    Litter bag: microbial biomass and CUE goes to zero, if not subject to a trickling input

--- R/cueSesam.R
+++ R/cueSesam.R
@@ -14,11 +14,12 @@ cueSesam <- function(
   # ans
   ##value<< input data.frame with computed columns
   a <- resSesam
-  a$cueDef =  a$synB / (a$uptakeC)  ##<<
+  synB0 <- pmax(0, a$synB)  # non-negative biomass synthesis
+  a$cueDef =  synB0 / (a$uptakeC)  ##<<
   ## biomass synthesis / uptake
   a$cueDB = (a$synB - a$tvrB - a$tvrBPred) / (a$synB - a$tvrB - a$tvrBPred + a$resp) ##<<
-  ## biomass change / (biomass change + resp )
-  a$cueSyn = (a$synB ) / (a$synB + a$resp) ##<<
+  # ## biomass change / (biomass change + resp )
+  a$cueSyn = (synB0 ) / (synB0 + a$resp) ##<<
   ## biomass synthesis / (biomass synthesis + resp )
   a
 }
