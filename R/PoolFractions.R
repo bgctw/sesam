6247e970ed217ec4a1d1f61fbe7197ef69304e1e
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2018-03-16 12:16:51 +0100

    MultiPoolFractions Object
    infrastructure for tracking several fractions or isotopes of an Element

---
+++ R/PoolFractions.R
@@ -0,0 +1,131 @@
+
+MultiPoolFractions_updateElement <- function(
+  ### set states for given element
+  .self       ##<< MultiPoolFractions object updated
+  , x         ##<< numeric vector of state variables
+  , element   ##<< scalar string of element to update
+  , poolStatesMap ##<< named list with string vector of state variable names
+    ## for each pool, see \code{\link{MultiPoolFractions_getPoolStatesMap}}
+){
+  #message("MultiPoolFractions_updateElement for element ", element)
+  poolNames <- names(poolStatesMap)
+  fracEl <- lapply(poolStatesMap, function(extNames){
+    structure(x[extNames], names = extNames )
+  })
+  .self$frac[poolNames] <- fracEl
+  units <- .self$units[[element]]
+  .self$tot[poolNames] <- sapply(poolNames, function(poolName){
+    sum(fracEl[[poolName]]*units)
+  })
+  .self
+}
+
+MultiPoolFractions_getPoolStatesMap <- function(
+  ### construct state variable names for pools devided into fractions
+  frac        ##<< numeric vector of fractions for element pools
+  , poolNames ##<< numeric vector of pools that are devided into these fractions
+){
+  ##value<< a list for each pool Name listing the state variable names
+  poolStatesMap <- structure(lapply( poolNames, function(poolName){
+    extNames <- paste(poolName, frac, sep = "_")
+  }), names = poolNames)
+}
+
+
+
+MultiPoolFractions_updateScalars <- function(
+  ### set states for given scalar
+  .self       ##<< MultiPoolFractions object updated
+  , x         ##<< numeric vector of state variables
+  , poolNames ##<< numeric vector of pools that are scalars
+){
+  # make sure list entry and scalar have the pool name
+  .self$frac[poolNames] <- structure(lapply(poolNames, function(poolName){
+    structure(x[poolName], names = poolName)}), names = poolNames)
+  .self$tot[poolNames] <- x[poolNames]
+  .self
+}
+
+### An object (list) with list \code{frac} and vector \code{tot}
+MulitPoolFractions <- list(
+  ##describe<<
+  className = "MultiPoolFractions"    ##<< string to identify the class
+  , frac = list()           ##<< list for each pool
+    ##  with a named numeric vector of state variables
+  , tot = numeric()         ##<< named numeric vector with sums for each pool
+  , updateElement = MultiPoolFractions_updateElement  ##<< function
+    ## to update frac and tot of fractions from given state vector
+  , updateScalars = MultiPoolFractions_updateScalars
+  , setX = function(.self, x) stop(
+    "need to set specific function, see createSesam4setX")
+  , poolNames = function(.self){ names(.self$frac) }
+  , stateNames = function(.self){
+    do.call(c, sapply(.self$frac, function(fraci) names(fraci)))  }
+  ##end<<
+)
+
+createMultiPoolFractions <- function(
+  ### construct a MultiPoolFractions object
+  units   ## list for each element with a named numeric vector of fractions
+  , setX  ##<< function to set state variable vector, see \code{\link{createSesam4setX}}
+){
+  pf <- MulitPoolFractions
+  pf$units <- units
+  pf$setX <- setX
+  pf
+}
+
+
+
+createSesam4setX <- function(
+  ### create a setter function for MultiPoolFfractions for Sesam4
+  units  ## list for C,N,P with a named numeric vector of fractions in these pools
+){
+  .scalarPools <- c("alpha")
+  required <- c("C","N","P")
+  iMissing <- which( !(required %in% names(units)) )
+  if (length(iMissing)) stop(
+     "need to provide units for fractions in ", required[iMissing])
+  .poolStatesMapC <- MultiPoolFractions_getPoolStatesMap(
+    names(units$C), c("B","R","L"))
+  .poolStatesMapN <- MultiPoolFractions_getPoolStatesMap(
+    names(units$N), c("RN","LN","I"))
+  .poolStatesMapP <- MultiPoolFractions_getPoolStatesMap(
+    names(units$P), c("RP","LP","IP"))
+  ##value<< a function that properly updates frac and tot in .self
+  function(.self,x){
+    .self <- .self$updateElement(.self, x, "C", .poolStatesMapC)
+    .self <- .self$updateElement(.self, x, "N", .poolStatesMapN)
+    .self <- .self$updateElement(.self, x, "P", .poolStatesMapP)
+    .self <- .self$updateScalars(.self, x, .scalarPools)
+    .self
+  }
+}
+
+createSesam4CNsetX <- function(
+  ### create a setter function for MultiPoolFfractions for Sesam4 for CN isotopes
+  units  ## list for C,N with a named numeric vector of fractions in these pools
+){
+  .scalarPools <- c("RP", "LP", "IP", "alpha")
+  required <- c("C","N")
+  iMissing <- which( !(required %in% names(units)) )
+  if (length(iMissing)) stop(
+    "need to provide units for fractions in ", required[iMissing])
+  .poolStatesMapC <- MultiPoolFractions_getPoolStatesMap(
+    names(units$C), c("B","R","L"))
+  .poolStatesMapN <- MultiPoolFractions_getPoolStatesMap(
+    names(units$N), c("RN","LN","I"))
+  ##value<< a function that properly updates frac and tot in .self
+  function(.self,x){
+    .self <- .self$updateElement(.self, x, "C", .poolStatesMapC)
+    .self <- .self$updateElement(.self, x, "N", .poolStatesMapN)
+    .self <- .self$updateScalars(.self, x, .scalarPools)
+    .self
+  }
+}
+
+
+
+
+
+
---
+++ tests/testthat/test_PoolFractions.R
@@ -0,0 +1,46 @@
+#require(testthat)
+#test_file("tests/testthat/test_PoolFractions.R")
+context("MultiPoolFractions")
+
+test_that("MultiPoolFractions amend through all fractions", {
+  elements <- c("C","N","P")
+  units <- structure(lapply(elements, function(el) c(SOM = 1, amend = 1))
+                     , names = elements)
+  x <- createMultiPoolFractions(units, setX = createSesam4setX(units))
+  expect_equal( x$units, units)
+  x <- x$setX(x, numeric())
+  expect_equal( x$poolNames(x), c("B","R","L","RN","LN","I","RP","LP","IP","alpha"))
+  expect_true( all( c("R_SOM","R_amend","RP_SOM","alpha") %in% x$stateNames(x) ))
+  #.self <- x  # rm(.self)  # poolName <- "RP"
+  x0 <- structure(seq_along(x$stateNames(x)), names = x$stateNames(x))
+  x <- x$setX(x,x0)
+  expect_equivalent( x$frac[["alpha"]], x0[c("alpha")])
+  expect_equivalent( x$frac[["RP"]], x0[c("RP_SOM","RP_amend")])
+  expect_equivalent( x$frac[["R"]], x0[c("R_SOM","R_amend")])
+  expect_equivalent( x$tot["alpha"], x0[c("alpha")])
+  expect_equivalent( x$tot["R"], sum(x0[c("R_SOM","R_amend")]*units$C))
+  expect_equivalent( x$tot["RN"], sum(x0[c("RN_SOM","RN_amend")]*units$N))
+})
+
+test_that("MultiPoolFractions 13C 14N, noPIso", {
+  units <- list(
+    C = c(C12 = 1, C13 = 0.01, C14 = 1e-12) # 13C in percent, 14C ppTrillion
+    , N = c(N14 = 1, N15 = 0.01) # 15N in percent
+  )
+  x <- createMultiPoolFractions(units, setX = createSesam4CNsetX(units))
+  x <- x$setX(x, numeric())
+  expect_equal( x$poolNames(x), c("B","R","L","RN","LN","I","RP","LP","IP","alpha"))
+  expect_true( all( c("R_C12","R_C13","RN_N14","alpha","RP") %in% x$stateNames(x) ))
+  #.self <- x  # rm(.self)  # poolName <- "RP"
+  x0 <- structure(seq_along(x$stateNames(x)), names = x$stateNames(x))
+  x <- x$setX(x,x0)
+  expect_equivalent( x$frac[["alpha"]], x0[c("alpha")])
+  expect_equivalent( x$frac[["RP"]], x0[c("RP")])
+  expect_equivalent( x$frac[["R"]], x0[c("R_C12","R_C13","R_C14")])
+  expect_equivalent( x$tot["alpha"], x0[c("alpha")])
+  expect_equivalent( x$tot["R"], sum(x0[c("R_C12","R_C13","R_C14")]*units$C))
+  expect_equivalent( x$tot["RN"], sum(x0[c("RN_N14","RN_N15")]*units$N))
+})
+
+
+
