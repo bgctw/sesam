e74510e886897c26f33523195d54ef698c1dfdd1
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2018-04-05 10:26:05 +0200

    litter bag simulation with higher litter inputs

--- R/cueSesam.R
+++ R/cueSesam.R
@@ -17,9 +17,9 @@ cueSesam <- function(
   synB0 <- pmax(0, a$synB)  # non-negative biomass synthesis
   a$cueDef =  synB0 / (a$uptakeC)  ##<<
   ## biomass synthesis / uptake
-  a$cueDB = (a$synB - a$tvrB - a$tvrBPred) / (a$synB - a$tvrB - a$tvrBPred + a$resp) ##<<
+  a$cueDB = (a$synB - a$tvrB - a$tvrBPred) / (a$synB - a$tvrB - a$tvrBPred + a$respTotal) ##<<
   # ## biomass change / (biomass change + resp )
-  a$cueSyn = (synB0 ) / (synB0 + a$resp) ##<<
+  a$cueSyn = (synB0 ) / (synB0 + a$respTotal) ##<<
   ## biomass synthesis / (biomass synthesis + resp )
   a
 }
